a331b97af0f447332a4f5713f8521add46c12c82
Alex <bethazari@gmail.com>
2019-04-08 00:43:35 +0300

    WIP: generation images

--- initializers/parser.js
+++ initializers/parser.js
@@ -1,7 +1,14 @@
 
+const fileType = require('file-type');
 const rp = require("request-promise");
 const $ = require("cheerio");
+
+const fs = require("fs");
+const path = require("path");
 const util = require("util");
+
+const writeFile = util.promisify(fs.writeFile);
+const readDir = util.promisify(fs.readdir);
 
 
 class AutoRuParserInitializer {
@@ -19,10 +26,13 @@ class AutoRuParserInitializer {
     for (let model of models) {
       generations = generations.concat(await this.loadGenerations(model));
     }
-    console.log(generations.length);
+    altha.logger.app.info("<==== Generations images caching ====>");
+    const images = await readDir(`${path.resolve(".")}/cdn/images`);
+    //for (let generation of generations) {
+      await this.loadImage(generations[0], images);
+    //}
 
     // # TODO: тут будет парсинг кузовов и цен по каждому - но только по выбранным юзером в интерфейсе моделям
-
   }
 
   async loadBrands() {
@@ -56,6 +66,7 @@ class AutoRuParserInitializer {
       altha.logger.app.info(`There are ${models.length} models for brand ${brand.name} in db!`);
       return models;
     } else {
+      altha.logger.app.info(`There are no models for brand ${brand.name} in db!`);
       const html = await this._getAutoRuPageHtml(brand.url);
       const parsedModels = $(".search-form-v2-list_type_popular > div > div > a", html)
         .map((i, link) => ({
@@ -80,6 +91,7 @@ class AutoRuParserInitializer {
       altha.logger.app.info(`There are ${generations.length} generations for model ${model.name} in db!`);
       return generations;
     } else {
+      altha.logger.app.info(`There are no generations for model ${model.name} in db!`);
       const html = await this._getAutoRuPageHtml(model.url);
       const parsedGenerations = $(".catalog-generation-summary__generations > div", html)
         .map((i, block) => {
@@ -106,6 +118,23 @@ class AutoRuParserInitializer {
     }
   }
 
+  async loadImage(generation, images) {
+    altha.logger.app.info(`Fetch image for generation ${generation.name} from fs...`);
+    const imageExists = images.find(imageName => imageName === generation.image_name);
+    if (!imageExists) {
+      altha.logger.app.info(`There is no image for generation ${generation.name} on fs!`);
+      const response = await this._getAutoRuImage(`http://${generation.image}`);
+      const imageName = `${generation.name}.${response.headers["content-type"].split("image/")[1]}`;
+      altha.logger.app.info(`Fetched image for generation ${generation.name}!`);
+      await writeFile(`${path.resolve(".")}/cdn/images/${imageName}`, response.body, "binary");
+      altha.logger.app.info(`Saving imagename into db...`);
+      await altha.mongo.main.db.collection("generations").updateOne(
+        { _id: new altha.helpers.utils.ObjectId(generation._id) },
+        { $set: { image_name: imageName, updated: new Date() } },
+      )
+    }
+  }
+
   async _getAutoRuPageHtml(url) {
     await this._delay(1000);
     return await rp({
@@ -113,6 +142,14 @@ class AutoRuParserInitializer {
       headers: {
         "Cookie": "los=1; los=1; bltsr=1; bltsr=1",
       },
+    });
+  }
+
+  async _getAutoRuImage(url) {
+    await this._delay(1000);
+    return await rp(url, {
+      encoding: "binary",
+      resolveWithFullResponse: true,
     });
   }
 
